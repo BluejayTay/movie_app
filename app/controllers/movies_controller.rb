2d330cfa9d01ae78f8aa2fe432c28a8b9ae02b5f
BluejayTay <tayrusthoven@gmail.com>
2022-01-28 16:53:42 -0600

    update movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,26 @@
 class MoviesController < ApplicationController
   def show
-    @movie = Movie.find[params[:api_id]]
+    binding.pry
+    @movie = MovieService.find_by_api_id((params["id"].to_i))
   end
+
   def popular
     @movies = MovieService.popular
   end
+
   def search_by_title
     query = params[:name]
 
     if query.present? 
       @movies = MovieService.title_search(query)
       render 'search_results'
-    else 
-      #put flash message re:error
+    else
       redirect_to '/movies/search'
     end
   end
+  def search
+    render 'search'
+  end
+  #def search_results
+  #end
 end
