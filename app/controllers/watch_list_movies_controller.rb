2ba50da731262276b8acae9008a16ba89aaeaa38
BluejayTay <tayrusthoven@gmail.com>
2022-01-29 18:22:24 -0600

    add whitelisting of movie_params for watch_list_movie

--- app/controllers/watch_list_movies_controller.rb
+++ app/controllers/watch_list_movies_controller.rb
@@ -3,12 +3,13 @@ class WatchListMoviesController < ApplicationController
   def index
     @watch_list_movies = current_user.watch_list_movies
   end
+
   def new
     @watch_list_movie = WatchListMovie.new
   end
+
   def create
-    binding.pry
-    movie = Movie.find_or_create_by(params[:api_id])
+    movie = Movie.find_or_create_by(movie_params)
     current_user.add_to_watchlist(movie)
     respond_to do |format|
         format.html
@@ -18,5 +19,10 @@ class WatchListMoviesController < ApplicationController
   
   def destroy
   end
- 
+
+  private
+
+  def movie_params
+    params.require(:watch_list_movie).permit(:api_id, :name, :image_url)
+  end
 end
--- app/models/watch_list_movie.rb
+++ app/models/watch_list_movie.rb
@@ -2,4 +2,5 @@ class WatchListMovie < ApplicationRecord
   belongs_to :user
   belongs_to :movie
   #validates :order, presence: true
+  validates_uniqueness_of :user_id, scope: :movie_id
 end
