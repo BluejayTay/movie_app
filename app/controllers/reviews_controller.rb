c4f41b4eaff182c4b51d316834df99a50465da7d
BluejayTay <tayrusthoven@gmail.com>
2022-02-01 14:34:31 -0600

    fix review controller and form to save reviews, add validation to review model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,57 @@
 class ReviewsController < ApplicationController
+  # require authentication w/ devise
+
   def index
-    @reviews = current_user.reviews.all
+    @reviews = current_user.reviews
   end
-  def show
-    @review = Review.find(params[:id])
-  end
+
   def new
     @review = Review.new
-    render 'review/new'
   end
+
   def create
+    binding.pry
+    @review = current_user.reviews.build(review_params)
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to reviews_path, notice: "Review was successfully created." }
+        format.json
+      else
+        format.html 
+        format.json 
+      end
+    end
   end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
   def update
+    @review = Review.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json
+    end
+    if @review.update(review_params)
+      flash[:notice] = "Review successfully edited!"
+      redirect_to reviews_path
+    else
+      render "edit"
+    end
   end
+
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:notice] = "Review successfully deleted!"
+    redirect_to reviews_path
   end
+  
+  private
+
+  def review_params
+    params.require(:review).permit(:user_id, :movie_id, :content, :rating)
+  end
+  
 end
