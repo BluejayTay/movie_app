4565ec2531e234615d1718ab962665112497d73b
BluejayTay <tayrusthoven@gmail.com>
2022-02-01 14:43:48 -0600

    fix spacing, add authentication before actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,14 @@
 class MoviesController < ApplicationController
+  before_action :authenticate_user!
   def show
-    
     @movie = MovieService.find_by_api_id(params["id"])
   end
+  
   def new
   end
+  
   def create
     @movie = Movie.new(movie_params)
-    
   end
 
   def popular
@@ -28,9 +29,12 @@ class MoviesController < ApplicationController
   def search
     render 'search'
   end
+  
   #def search_results
   #end
+  
   private
+  
   def movie_params
     params.require(:movie).permit(:api_id, :name, :image_url)
   end
