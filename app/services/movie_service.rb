5c6aa1ff616723286d2eda80ae42222023ed0f55
BluejayTay <tayrusthoven@gmail.com>
2022-02-02 14:58:21 -0600

    clean up MovieService

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -9,15 +9,6 @@ class MovieService
   #survive.","popularity":824.127,"poster_path":"/d5NXSklXo0qyIYkgV94XAgMIckC.jpg","release_date":"2021-09-15",
   #"title":"Dune","video":false,"vote_average":7.9,"vote_count":5664}
   
-  #configuration = {"images":{"base_url":"http://image.tmdb.org/t/p/","secure_base_url":"https://image.tmdb.org/t/p/",
-  #"backdrop_sizes":["w300","w780","w1280","original"],"logo_sizes":["w45","w92","w154","w185","w300","w500","original"],
-  #"poster_sizes":["w92","w154","w185","w342","w500","w780","original"],"profile_sizes":["w45","w185","h632","original"],
-  #"still_sizes":["w92","w185","w300","original"]},"change_keys":["adult","air_date","also_known_as","alternative_titles",
-  #"biography","birthday","budget","cast","certifications","character_names","created_by","crew","deathday","episode","episode_number",
-  #"episode_run_time","freebase_id","freebase_mid","general","genres","guest_stars","homepage","images","imdb_id","languages","name","network",
-  #"origin_country","original_name","original_title","overview","parts","place_of_birth","plot_keywords","production_code","production_companies",
-  #"production_countries","releases","revenue","runtime","season","season_number","season_regular","spoken_languages","status","tagline","title",
-  #"translations","tvdb_id","tvrage_id","type","video","videos"]}
   #{"genres":[{"id":28,"name":"Action"},{"id":12,"name":"Adventure"},{"id":16,"name":"Animation"},{"id":35,"name":"Comedy"},
   #{"id":80,"name":"Crime"},{"id":99,"name":"Documentary"},{"id":18,"name":"Drama"},{"id":10751,"name":"Family"},{"id":14,"name":"Fantasy"},
   #{"id":36,"name":"History"},{"id":27,"name":"Horror"},{"id":10402,"name":"Music"},{"id":9648,"name":"Mystery"},{"id":10749,"name":"Romance"},
@@ -26,11 +17,14 @@ class MovieService
     response = HTTParty.get("https://api.themoviedb.org/3/discover/movie?api_key=" + "#{ENV['TMDB_api_key']}" + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1");
     response["results"];
   end
+
   def self.title_search(query)
     response = HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=" + "#{ENV['TMDB_api_key']}" + "&language=en-US&query=" + "#{query}" + "&page=1&include_adult=false");
     response["results"];
   end
+
   def self.find_by_api_id(api_id)
     response = HTTParty.get("https://api.themoviedb.org/3/movie/" + "#{api_id}" + "?api_key=" + "#{ENV['TMDB_api_key']}" + "&language=en-US");
   end
+
 end
