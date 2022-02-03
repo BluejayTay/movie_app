066990698f636bafb0c83ff1fe15703d029d2072
BluejayTay <tayrusthoven@gmail.com>
2022-02-03 17:35:57 -0600

    add functionality to modify priority for watch_list_movies

--- app/controllers/watch_list_movies_controller.rb
+++ app/controllers/watch_list_movies_controller.rb
@@ -2,7 +2,7 @@ class WatchListMoviesController < ApplicationController
   before_action :authenticate_user!
 
   def index  
-    @watch_list_movies = current_user.watch_list_movies
+    @watch_list_movies = current_user.watch_list_movies.order(order: :desc)
   end
 
   def new
@@ -12,12 +12,22 @@ class WatchListMoviesController < ApplicationController
   def create
     movie = Movie.find_or_create_by(movie_params)
     current_user.add_to_watchlist(movie)
+
     respond_to do |format|
         format.html
         format.json
     end
+
     flash[:notice] = "Movie successfully added to your watchlist!"
     redirect_to movie_path(id: movie.api_id)
+  end
+
+  def update
+    @watch_list_movie = WatchListMovie.find(params[:id])
+
+    if @watch_list_movie.update(watch_list_movie_params)
+      redirect_to watch_list_movies_path
+    end
   end
   
   def destroy
@@ -31,5 +41,8 @@ class WatchListMoviesController < ApplicationController
 
   def movie_params
     params.require(:watch_list_movie).permit(:api_id, :name, :image_url)
+  end
+  def watch_list_movie_params
+    params.require(:watch_list_movie).permit(:order)
   end
 end
