1134d0f331650fe7852edd2488d08be1352b7b90
BluejayTay <tayrusthoven@gmail.com>
2022-02-07 20:42:14 -0600

    adjust image sizing, bootstrap configuration

--- app/frontend/js/bootstrap_js_files.js
+++ app/frontend/js/bootstrap_js_files.js
@@ -1,7 +1,7 @@
 // inside app/frontend/js/bootstrap_js_files.js  
   
-// import 'bootstrap/js/src/alert'  
-// import 'bootstrap/js/src/button'  
+import 'bootstrap/js/src/alert'  
+import 'bootstrap/js/src/button'  
 // import 'bootstrap/js/src/carousel'  
 import 'bootstrap/js/src/collapse'  
 import 'bootstrap/js/src/dropdown'  
