2e4796f1100b72634b20253e5fc7838646daa322
BluejayTay <tayrusthoven@gmail.com>
2022-02-11 10:57:42 -0600

    add postinstall script to package.json for deployment bug

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,8 +1,3 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-
-const environment = require('./environment')
-
-module.exports = environment.toWebpackConfig()
 process.env.NODE_ENV = process.env.NODE_ENV || "production";
 
 const environment = require("./environment");
