eeb190f95d018fca1479c273677ba3e3804484f1
BluejayTay <tayrusthoven@gmail.com>
2022-02-10 20:34:40 -0600

    adds movie model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,6 @@ class Movie < ApplicationRecord
   has_many :reviews
   has_many :watch_list_movies
   has_many :users, through: :watch_list_movies
-  #validates :name, :image_url, :api_id, presence: true
+  validates :name, :image_url, :api_id, presence: true
 
 end
