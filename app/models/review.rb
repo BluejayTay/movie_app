6effea797e04e8638581573191ab628a0494de2f
BluejayTay <tayrusthoven@gmail.com>
2022-02-12 13:41:42 -0600

    adds content length validation for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,6 @@ class Review < ApplicationRecord
   belongs_to :movie
   validates_uniqueness_of :user_id, scope: :movie_id
   validates :rating, presence: true
+  validates :content, length: { maximum: 570,
+    too_long: "%{count} characters is the maximum allowed" }
 end
