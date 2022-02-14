9f836453f0e661db4a5525663fba5f0580268aef
BluejayTay <tayrusthoven@gmail.com>
2022-02-14 13:42:13 -0600

    fixes local bootstrap bug

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,8 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap'
-import '../stylesheets/application'
+import '../js/bootstrap_js_files.js'
 
 require.context('../images', true)
 
