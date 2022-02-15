1f49c5f3f9a07c7bfe4992d5d799ecf7c79a7140
BluejayTay <tayrusthoven@gmail.com>
2022-02-15 14:24:57 -0600

    adds content presence validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
   validates_uniqueness_of :user_id, scope: :movie_id
-  validates :rating, presence: true
+  validates :rating, :content, presence: true
   validates :content, length: { maximum: 570,
                                 too_long: '570 characters is the maximum allowed' }
 end
